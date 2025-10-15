a17f3be79e244ee57071847a157059ab6093e767
unknown <skonby_jonas@hotmail.com>
2025-10-15 20:09:32 +0200

    first commit

---
+++ src/main/java/com/EventApplication/EventApplication/EventApplication.java
@@ -0,0 +1,13 @@
+package com.EventApplication.EventApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventApplication.class, args);
+	}
+
+}
